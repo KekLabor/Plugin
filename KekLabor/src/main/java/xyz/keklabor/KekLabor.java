4cffe686e8b8ff89db9295a6782f9952a66ad3a7
WinniePatGG <wc92226@gmail.com>
2025-04-11 13:31:48 +0200

    v1

--- KekLabor/src/main/java/xyz/keklabor/KekLabor.java
+++ KekLabor/src/main/java/xyz/keklabor/KekLabor.java
@@ -1,9 +1,11 @@
 package xyz.keklabor;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import xyz.keklabor.commands.ManageCommand;
 import xyz.keklabor.commands.MusicCommand;
 import xyz.keklabor.commands.SpawnCommand;
 import xyz.keklabor.listeners.CatMusicListener;
+import xyz.keklabor.listeners.ManageInventoryListener;
 import xyz.keklabor.listeners.MusicInventoryListener;
 import xyz.keklabor.music.CatMusicPlayer;
 
@@ -34,10 +36,12 @@ public final class KekLabor extends JavaPlugin {
     private void registerCommands() {
         Objects.requireNonNull(getCommand("spawn")).setExecutor(new SpawnCommand());
         Objects.requireNonNull(getCommand("music")).setExecutor(new MusicCommand(musicPlayer));
+        Objects.requireNonNull(getCommand("manage")).setExecutor(new ManageCommand());
     }
 
     private void registerListeners() {
         getServer().getPluginManager().registerEvents(new CatMusicListener(musicPlayer, this), this);
         getServer().getPluginManager().registerEvents(new MusicInventoryListener(musicPlayer), this);
+        getServer().getPluginManager().registerEvents(new ManageInventoryListener(), this);
     }
 }
